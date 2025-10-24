d292e3cb6ea3b6adb7f6a5b7ad4e838c261b5518
Ayman Abusura <aymansalameh1996@hotmail.com>
2025-10-24 14:07:43 +0200

    Done

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,9 @@ const WEBLINK = process.env.WEB_LINK;
 const bot = new Telegraf(TOKEN);
 
 bot.start((ctx) =>
-  ctx.reply("Welcome!", {
+  ctx.reply(
+    "📈 Welcome to your first Telegram bot that lets you earn real money! Earn real money and withdraw it to your bank account! Available to everyone!", 
+    {
     reply_markup: {
       inline_keyboard: [[{ text: "Go to Mini App 🚀", web_app: { url: WEBLINK } }]],
     },
